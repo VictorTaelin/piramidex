4cdf1af693e65295a7fd1dd4b5e907ec5d6e1498
Victor Maia <srvictormaia@gmail.com>
2018-03-07 18:30:28 -0300

    Adjust styles

--- index.js
+++ index.js
@@ -286,7 +286,7 @@ const Piramidex = createClass({
       className={this.state.blink ? "blink" : ""}
       style={{
         width:"100%",
-        height:"100%",
+        minHeight:"100vh",
         backgroundColor: "#FAFAFA",
         color: "#505154",
       }}>
